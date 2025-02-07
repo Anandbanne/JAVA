1b4927d2bb7881ccf73ee03e5bc71af1d7d1b127
Anand Banne <anandbanne43@gmail.com>
2025-02-07 12:00:01 +0530

    Add files via upload

---
+++ oprtn.java
@@ -0,0 +1,21 @@
+import java.lang.Math;
+public class oprtn
+{
+ public static void main(String args[])
+ {
+  int a=3;
+  int b=6;
+  int c=a+b;
+  int d=a*b;
+  int e=a%b;
+  int f=a/b;
+  int g=a-b;
+  int x=a+b+c+e+d+f+g;
+  System.out.println("Sum of two nmbers are :"+c);
+  System.out.println("mul of two nmbers are :"+d);
+  System.out.println("mod of two nmbers are :"+e);
+  System.out.println("div of two nmbers are :"+f);
+  System.out.println("sub of two nmbers are :"+g);
+  System.out.println("sum of all nmbers are :"+x);
+ }
+} 
