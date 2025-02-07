55839dc6a5300711d574b0f7dffa3095d0958560
Anandbanne <anandbanne43@gmail.com>
2025-02-07 11:37:04 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+ import java.lang.Math;
+ public class Hello
+ {
+  public static void main(String args[])
+  {
+   double y=36;
+   double x=Math.sqrt(y);
+   System.out.println("x ="+x);
+  }
+ }
+ 
+  
